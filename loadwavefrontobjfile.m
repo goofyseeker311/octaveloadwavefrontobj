2e672cee77fa7bd5c9aaf596eccac9fa4f5754fe
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-13 19:21:41 +0300

    added str2num() fix on the faceindex values
    removed comments and clutter, removed smoothing groups

--- loadwavefrontobjfile.m
+++ loadwavefrontobjfile.m
@@ -1,19 +1,15 @@
-###.obj loader tags: #,mtllib,o,v,vn,vt,sm,usemtl,f
 function [k] = loadwavefrontobjfile(fname)
-  k = []; k.objects = {}; fhandle = fopen(fname); ##read only default
-  fline = []; objectcount = 0; smgroupcount = 0; flinecount = 0;
+  k = []; k.objects = {}; fhandle = fopen(fname);
+  fline = []; objectcount = 0; flinecount = 0;
   while((fline=fgets(fhandle))!=-1)
     fline = strtrim(fline);
-    #fline = substr(fline,1,length(fline));
-    #printf("%s\n",fline); #fline
     if (strncmp(lower(fline),"#",1))
-      ##comment ignored
     elseif (strncmp(lower(fline),"mtllib ",7))
       farg = strtrim(substr(fline, 8)); k.mtllib = farg;
       k.materials = loadwavefrontmtlfile(farg).materials;
     elseif (strncmp(lower(fline),"o ",2))
       farg = strtrim(substr(fline, 3));
-      objectcount += 1; smgroupcount = 0;
+      objectcount += 1;
       k.objects{objectcount}.object = farg;
       k.objects{objectcount}.vertexlist = [];
       k.objects{objectcount}.facenormals = [];
@@ -31,13 +27,9 @@ function [k] = loadwavefrontobjfile(fname)
       farg = strtrim(substr(fline, 4));
       vtline = str2num(farg);
       k.objects{objectcount}.texturecoords(end+1,:) = vtline;
-    elseif (strncmp(lower(fline),"s ",2))
-      farg = strtrim(substr(fline, 3));
-      smgroupcount += 1; flinecount = 0;
-      k.objects{objectcount}.smoothgroups{smgroupcount}.faceindex = {};
     elseif (strncmp(lower(fline),"usemtl ",7))
       farg = strtrim(substr(fline, 8));
-      k.objects{objectcount}.smoothgroups{smgroupcount}.usemtl = farg;
+      k.objects{objectcount}.usemtl = farg;
     elseif (strncmp(lower(fline),"f ",2))
       farg = strtrim(substr(fline, 3));
       fcline = strsplit(farg,{" "});
@@ -46,11 +38,11 @@ function [k] = loadwavefrontobjfile(fname)
         fclinecol = strsplit(fcline{n},{"/"});
         fclinecolmat = [];
         for m = 1:size(fclinecol,2)
-          fclinecolmat(1,m) = fclinecol{m};
+          fclinecolmat(1,m) = str2num(fclinecol{m});
         endfor
         fclinemat{n} = fclinecolmat;
       endfor
-      k.objects{objectcount}.smoothgroups{smgroupcount}.faceindex{flinecount} = fclinemat;
+      k.objects{objectcount}.faceindex{flinecount} = fclinemat;
     endif
   endwhile
   fclose(fhandle);
