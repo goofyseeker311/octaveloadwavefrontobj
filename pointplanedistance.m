820c6a8a2efa0cd31300dbf303d20c61b506cc12
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-18 17:16:50 +0300

    nan array instead of div by zero

--- pointplanedistance.m
+++ pointplanedistance.m
@@ -1,4 +1,4 @@
 function [k] = pointplanedistance(vpoint,vplane)
-  k = dot([vpoint 1],vplane)./sqrt(dot(vplane(1:3),vplane(1:3)));
+  k = nan(size(vpoint)); l2 = dot(vplane(1:3),vplane(1:3));
+  if (l2!=0) k = dot([vpoint 1],vplane)./sqrt(l2); endif;
 endfunction
-
