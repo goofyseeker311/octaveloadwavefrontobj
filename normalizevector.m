61f2ed38567b418e61447e82d5f10ac6374da271
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-18 16:42:03 +0300

    changed to nan array instead of empty

--- normalizevector.m
+++ normalizevector.m
@@ -1,3 +1,3 @@
 function [k] = normalizevector(vdir)
-  k = []; l2 = dot(vdir,vdir); if (l2!=0) k = vdir./sqrt(l2); endif;
+  k = nan(size(vdir)); l2 = dot(vdir,vdir); if (l2!=0) k = vdir./sqrt(l2); endif;
 endfunction
