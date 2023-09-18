7680db3e99fc9dbbd993e2c856bc5792de7c7ef9
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-18 17:32:01 +0300

    nan array instead of empty

--- rayplanedistance.m
+++ rayplanedistance.m
@@ -1,4 +1,4 @@
 function [k] = rayplanedistance(vpos,vdir,vplane)
-  k = []; top = -dot([vpos 1],vplane); bottom = dot(vdir,vplane(1:3));
-  if (bottom!=0) k = top./bottom; endif
+  k = nan(size(vpos)); top = -dot([vpos 1],vplane);
+  bottom = dot(vdir,vplane(1:3)); if (bottom!=0) k = top./bottom; endif
 endfunction
