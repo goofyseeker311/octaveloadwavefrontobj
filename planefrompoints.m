9be4dd8a9ec92c244d81cdddbefc196f9ef35ac8
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-18 16:54:39 +0300

    uses normalizevector now

--- planefrompoints.m
+++ planefrompoints.m
@@ -2,7 +2,7 @@ function [k] = planefrompoints(points)
   v1 = points(2,:)-points(1,:);
   v2 = points(3,:)-points(1,:);
   nm = cross(v1,v2);
-  nm = nm./sqrt(dot(nm,nm));
+  nm = normalizevector(nm);
   dv = -dot(nm,points(1,:));
   k = [nm dv];
 endfunction
