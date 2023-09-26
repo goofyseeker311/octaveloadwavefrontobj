4311b748f8620f9b922237a7f30c441007213564
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-26 03:56:30 +0300

    updated algorithm and added uv-coordinates
    fixed some cases where plane and triangle side were in same alignment

--- planetriangleintersection.m
+++ planetriangleintersection.m
@@ -1,20 +1,14 @@
-function [k,d] = planetriangleintersection(vplane,vtriangle)
-  k = nan; d = false;
-  tdist1 = pointplanedistance(vtriangle(1,:),vplane);
-  tdist2 = pointplanedistance(vtriangle(2,:),vplane);
-  tdist3 = pointplanedistance(vtriangle(3,:),vplane);
-  tdistL = [tdist1 tdist2 tdist3]; [tdistlist,tdistinds] = sort(tdistL);
-  mintdist = tdistlist(1); maxtdist = tdistlist(3); medtdist = tdistlist(2);
-  mintdind = tdistinds(1); maxtdind = tdistinds(3); medtdind = tdistinds(2);
-  if ((mintdist<=0)&&(maxtdist>=0))
-    rootind = mintdind; if (medtdist<0) rootind = maxtdind; endif
-    otherind = setdiff([1 2 3],rootind); rootpoint = vtriangle(rootind,:);
-    tv1 = vtriangle(otherind(1),:)-rootpoint;
-    tv2 = vtriangle(otherind(2),:)-rootpoint;
-    tvrd = abs(tdistL(rootind));
-    tv1d = abs(tdistL(otherind(1))); tv2d = abs(tdistL(otherind(2)));
-    tv1m = (tvrd./(tvrd + tv1d)); tv2m = (tvrd./(tvrd + tv2d));
-    tv1p = tv1m.*tv1 + rootpoint; tv2p = tv2m.*tv2 + rootpoint;
-    k = [tv1p; tv2p]; d = true;
+function [k,d,u,f] = planetriangleintersection(vplane,vtriangle)
+  k = nan; d = false; u = nan(2,1); f = nan(2,1);
+  [ptlint1,ptlhit1,ptluv1] = planelineintersection(vplane,vtriangle([1 2],:));
+  [ptlint2,ptlhit2,ptluv2] = planelineintersection(vplane,vtriangle([1 3],:));
+  [ptlint3,ptlhit3,ptluv3] = planelineintersection(vplane,vtriangle([2 3],:));
+  if (ptlhit1||ptlhit2||ptlhit3)
+    ptint = [];
+    if (ptlhit1) ptint = [ptint;ptlint1 ptluv1 ones(size(ptlint1,1),1)*[1,2]]; endif
+    if (ptlhit2) ptint = [ptint;ptlint2 ptluv2 ones(size(ptlint2,1),1)*[1,3]]; endif
+    if (ptlhit3) ptint = [ptint;ptlint3 ptluv3 ones(size(ptlint3,1),1)*[2,3]]; endif
+    ptint = unique(ptint,"rows");
+    k = ptint(:,1:3); d = true; u = ptint(:,4); f = ptint(:,5:6);
   endif
 endfunction
