942d93b1f87a9da66eaea1e018186c2619fc1567
goofyseeker311 <goofyseeker311@gmail.com>
2023-09-27 03:06:31 +0300

    simplified outputs

--- equilateralspheremaprays.m
+++ equilateralspheremaprays.m
@@ -1,11 +1,11 @@
-function [k,d] = equilateralspheremaprays(hres,vres)
-  k = []; d = []; hanglestep = 360./(hres-1); vanglestep = 180./(vres-1);
+function [k] = equilateralspheremaprays(hres,vres)
+  k = []; hanglestep = 360./(hres-1); vanglestep = 180./(vres-1);
   hanglelist = -180:hanglestep:180; vanglelist = -90:vanglestep:90;
   vvectors = [cosd(vanglelist);zeros(1,size(vanglelist,2));sind(vanglelist)];
   rvectors = zeros(3,vres,hres);
   for n = 1:hres
     svectors = (rotationmatrix(0,0,hanglelist(n))*vvectors)';
     svectors = normalizevector(svectors);
-    k(:,:,n) = svectors; d(end+1:end+vres,:) = svectors;
+    k(:,:,n) = svectors;
   endfor
 endfunction
--- objreader.m
+++ objreader.m
@@ -1,12 +1,12 @@
 clear all; format long; output_precision(16);
-hres = 32; vres = 32; objfilename = "testcubemodel4.obj";
+hres = 1024; vres = 1024; objfilename = "testcubemodel4.obj";
 campos = [-2 0.5 1.5]; camdir = normalizevector([1 0 -0.4]);
 camrgt = [0 -1 0]; camup = normalizevector(cross(camrgt,camdir));
 camplane = planefromnormalatpoint(campos,camup);
 triangle = [1 1 0;1 0 1;1 0 0];
 veclen = vectorlength(camdir);
 cubemodel = loadwavefrontobjfile(objfilename);
-[spheremaprays,smr] = equilateralspheremaprays(hres,vres);
+spheremaprays = equilateralspheremaprays(hres,vres);
 trplane = planefrompoints(triangle);
 ppdist = pointplanedistance(campos,trplane);
 cpdist1 = pointplanedistance(triangle(1,:),camplane);
