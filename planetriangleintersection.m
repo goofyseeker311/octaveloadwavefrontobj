0e4aa8f03267907f9c143b63986c85f71a25dfe6
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-02 04:25:55 +0300

    updated single output
    fixed when output was only single row, to always be two rows, equal lines

--- planetriangleintersection.m
+++ planetriangleintersection.m
@@ -9,6 +9,7 @@ function [k,d,u,f] = planetriangleintersection(vplane,vtriangle)
     if (ptlhit2) ptint = [ptint;ptlint2 ptluv2 ones(size(ptlint2,1),1)*[1,3]]; endif
     if (ptlhit3) ptint = [ptint;ptlint3 ptluv3 ones(size(ptlint3,1),1)*[2,3]]; endif
     [ptuint,ptuind] = unique(ptint(:,1:4),"rows"); puintf = [ptint(ptuind,:)];
+    if (size(puintf,1)) puintf = [puintf;puintf]; endif
     k = puintf(:,1:3); d = true; u = puintf(:,4); f = puintf(:,5:6);
   endif
 endfunction
