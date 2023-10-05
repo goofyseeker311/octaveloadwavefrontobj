649045153be55f0425f1dbff1d8931445708bcaf
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 07:28:50 +0300

    updated planefromnormalatpoint
    added support for multiple stacked input points and normals

--- planefromnormalatpoint.m
+++ planefromnormalatpoint.m
@@ -1,3 +1,10 @@
 function [k] = planefromnormalatpoint(vpoint,vnormal)
-  nm = normalizevector(vnormal); dv = -dot(nm,vpoint); k = [nm dv];
+  vpointc = size(vpoint,1); vnormalc = size(vnormal,1); k = nan;
+  if ((vpointc>0)&&(vnormalc>0))
+    k = nan(vpointc,4,vnormalc);
+    for n = 1:vnormalc
+      nm=ones(vpointc,1).*normalizevector(vnormal(n,:)); dv=-dot(nm,vpoint,2);
+      k(:,:,n) = [nm dv];
+    endfor
+  endif
 endfunction
