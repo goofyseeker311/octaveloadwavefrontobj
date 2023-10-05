506bb027f879e5879553048392515654bff807ac
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 05:22:17 +0300

    updated pointplanedistance
    added some invalid/empty inputs checking

--- pointplanedistance.m
+++ pointplanedistance.m
@@ -1,9 +1,12 @@
 function [k] = pointplanedistance(vpoint,vplane)
-  l2=vectorlength(vplane(:,1:3)); pointc=size(vpoint,1); planec=size(vplane,1);
-  k = nan(pointc,planec);
-  for n = 1:planec
-    if (l2(n)!=0)
-      k(:,n)=dot([vpoint ones(pointc,1)],ones(pointc,1).*vplane(n,:),2)./l2(n);
-    endif
-  endfor
+  pointc=size(vpoint,1); planec=size(vplane,1); k = nan;
+  if ((pointc>0)&&(planec>0))
+    k = nan(pointc,planec);
+    l2=vectorlength(vplane(:,1:3));
+    for n = 1:planec
+      if (l2(n)!=0)
+        k(:,n)=dot([vpoint ones(pointc,1)],ones(pointc,1).*vplane(n,:),2)./l2(n);
+      endif
+    endfor
+  endif
 endfunction
