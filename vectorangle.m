fe300489c00c87fd7b8608da76d34425d1a6651c
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 05:42:30 +0300

    updated vectorangle
    added support for multiple stacked vector inputs

--- vectorangle.m
+++ vectorangle.m
@@ -1,4 +1,7 @@
 function [k] = vectorangle(vdir1,vdir2)
-  k = nan; vl1 = vectorlength(vdir1); vl2 = vectorlength(vdir2);
-  if ((vl1!=0)&&(vl2!=0)) k = acosd(dot(vdir1,vdir2)./(vl1.*vl2)); endif
+  k = nan; vdir1c = size(vdir1,1); vdir2c = size(vdir2,1);
+  if (vdir1c>0)&&(vdir2c>0)&&(vdir1c==vdir2c)
+    vl1 = vectorlength(vdir1); vl2 = vectorlength(vdir2);
+    k = acosd(dot(vdir1,vdir2,2)./(vl1.*vl2)); k(find(isinf(k))) = nan;
+  endif
 endfunction
