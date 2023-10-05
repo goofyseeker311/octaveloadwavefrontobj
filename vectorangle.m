1234fa9c630d566358659b4dece2efae4395f445
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 06:28:44 +0300

    updated vectorangle
    inside a function number div by 0 is always nan already

--- vectorangle.m
+++ vectorangle.m
@@ -2,6 +2,6 @@ function [k] = vectorangle(vdir1,vdir2)
   k = nan; vdir1c = size(vdir1,1); vdir2c = size(vdir2,1);
   if (vdir1c>0)&&(vdir2c>0)&&(vdir1c==vdir2c)
     vl1 = vectorlength(vdir1); vl2 = vectorlength(vdir2);
-    k = acosd(dot(vdir1,vdir2,2)./(vl1.*vl2)); k(find(isinf(k))) = nan;
+    k = acosd(dot(vdir1,vdir2,2)./(vl1.*vl2));
   endif
 endfunction
