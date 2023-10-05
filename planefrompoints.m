830af9da966f780ea774412a5b032d0baf2899ad
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 07:03:38 +0300

    updated planefrompoints
    added support for multiple stacked point sets

--- planefrompoints.m
+++ planefrompoints.m
@@ -1,8 +1,15 @@
 function [k] = planefrompoints(points)
-  v1 = points(2,:)-points(1,:);
-  v2 = points(3,:)-points(1,:);
-  nm = cross(v1,v2);
-  nm = normalizevector(nm);
-  dv = -dot(nm,points(1,:));
-  k = [nm dv];
+  k = nan;
+  if (!isempty(points))
+    pointsc = size(points,3);
+    k = nan(pointsc,4);
+    for n = 1:pointsc
+      v1 = points(2,:,n)-points(1,:,n);
+      v2 = points(3,:,n)-points(1,:,n);
+      nm = cross(v1,v2);
+      nm = normalizevector(nm);
+      dv = -dot(nm,points(1,:,n));
+      k(n,:) = [nm dv];
+    endfor
+  endif
 endfunction
