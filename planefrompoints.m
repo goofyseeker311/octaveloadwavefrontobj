84b16e6f2d09ab13443e53c30f11ead890bd78a1
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 09:13:16 +0300

    updated planefrompoints
    updated to faster array processing (instead of slow loop)

--- planefrompoints.m
+++ planefrompoints.m
@@ -2,14 +2,15 @@ function [k] = planefrompoints(points)
   k = nan;
   if (!isempty(points))
     pointsc = size(points,3);
-    k = nan(pointsc,4);
-    for n = 1:pointsc
-      v1 = points(2,:,n)-points(1,:,n);
-      v2 = points(3,:,n)-points(1,:,n);
-      nm = cross(v1,v2);
-      nm = normalizevector(nm);
-      dv = -dot(nm,points(1,:,n));
-      k(n,:) = [nm dv];
-    endfor
+    xvpos = reshape(points(1,:,:),3,pointsc)';
+    yvpos = reshape(points(2,:,:),3,pointsc)';
+    zvpos = reshape(points(3,:,:),3,pointsc)';
+    vpos = [xvpos yvpos zvpos];
+    v1 = vpos(:,4:6)-vpos(:,1:3);
+    v2 = vpos(:,7:9)-vpos(:,1:3);
+    nm = cross(v1,v2,2);
+    nm = normalizevector(nm);
+    dv = -dot(nm,vpos(:,1:3),2);
+    k = [nm dv];
   endif
 endfunction
