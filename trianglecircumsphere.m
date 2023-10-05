80da650643190f63df09ba0d2b148c699f801452
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 06:08:33 +0300

    updated trianglecircumsphere
    added support for multiple stacked triangles (slow loop)

--- trianglecircumsphere.m
+++ trianglecircumsphere.m
@@ -1,12 +1,18 @@
 function [k] = trianglecircumsphere(triangle)
-  k = nan(1,4);
-  v12 = triangle(2,:)-triangle(1,:); v13 = triangle(3,:)-triangle(1,:);
-  v12l = vectorlength(v12).^2; v13l = vectorlength(v13).^2;
-  cv12v13 = cross(v12,v13); top = cross(v12l.*v13-v13l.*v12,cv12v13);
-  bottom = 2.*vectorlength(cv12v13).^2; p1 = triangle(1,:);
-  if (bottom!=0)
-    spherecenter = top./bottom + p1;
-    sphereradius = vectorlength(spherecenter - p1);
-    k = [spherecenter sphereradius];
+  k = nan;
+  if (!isempty(triangle))
+    trianglec = size(triangle,3);
+    k = nan(trianglec,4);
+    for n = 1:trianglec
+      v12=triangle(2,:,n)-triangle(1,:,n); v13=triangle(3,:,n)-triangle(1,:,n);
+      v12l = vectorlength(v12).^2; v13l = vectorlength(v13).^2;
+      cv12v13 = cross(v12,v13); top = cross(v12l.*v13-v13l.*v12,cv12v13);
+      bottom = 2.*vectorlength(cv12v13).^2; p1 = triangle(1,:,n);
+      if (bottom!=0)
+        spherecenter = top./bottom + p1;
+        sphereradius = vectorlength(spherecenter - p1);
+        k(n,:) = [spherecenter sphereradius];
+      endif
+    endfor
   endif
 endfunction
