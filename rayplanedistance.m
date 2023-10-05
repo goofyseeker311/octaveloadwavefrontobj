4f86c8230f04f5194f997a092cf9d15ac468edef
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 09:44:45 +0300

    updated rayplanedistance
    added support for multiple stacked ray directions (fast array) and planes (slow loop)

--- rayplanedistance.m
+++ rayplanedistance.m
@@ -1,4 +1,11 @@
 function [k] = rayplanedistance(vpos,vdir,vplane)
-  k = nan(size(vpos)); top = -dot([vpos 1],vplane);
-  bottom = dot(vdir,vplane(1:3)); if (bottom!=0) k = top./bottom; endif
+  vdirc = size(vdir,1); vplanec = size(vplane,1); k = nan;
+  if ((!isempty(vpos))&&(vdirc>0)&&(vplanec>0))
+    k = nan(vdirc,vplanec);
+    for n = 1:vplanec
+      top = -dot([vpos 1],vplane(n,:),2);
+      bottom = dot(vdir,ones(vdirc,1).*vplane(n,1:3),2);
+      k(:,n) = top./bottom;
+    endfor
+  endif
 endfunction
