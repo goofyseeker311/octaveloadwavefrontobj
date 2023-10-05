6fbf4545fa6efd4f3a91aac3305b9b99ff86b82e
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-05 05:14:11 +0300

    updated pointplanedistance
    added support for multiple stacked points and planes as arguments and outputs, as input sized matrix

--- pointplanedistance.m
+++ pointplanedistance.m
@@ -1,4 +1,9 @@
 function [k] = pointplanedistance(vpoint,vplane)
-  k = nan(size(vpoint)); l2 = vectorlength(vplane(1:3));
-  if (l2!=0) k = dot([vpoint 1],vplane)./l2; endif;
+  l2=vectorlength(vplane(:,1:3)); pointc=size(vpoint,1); planec=size(vplane,1);
+  k = nan(pointc,planec);
+  for n = 1:planec
+    if (l2(n)!=0)
+      k(:,n)=dot([vpoint ones(pointc,1)],ones(pointc,1).*vplane(n,:),2)./l2(n);
+    endif
+  endfor
 endfunction
