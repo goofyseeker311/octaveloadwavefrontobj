91ddb508c1160d1ebe0d6fad3f938dc755959a46
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-06 05:30:09 +0300

    updated renderobjectrayscamera
    turned off verbose mode, set ray collision greater than zero instead of equal-greater than zero, set background color to zero-black

--- renderobjectrayscamera.m
+++ renderobjectrayscamera.m
@@ -1,6 +1,6 @@
 function [k,d] = renderobjectrayscamera(vscene,vpos,vrays)
-  vhres = size(vrays,2); vvres = size(vrays,1); distlmod=10;vverb=true;
-  drawbuffer=0.5.*ones(vvres,vhres,3);zbuffer=inf(vvres,vhres);
+  vhres = size(vrays,2); vvres = size(vrays,1); distlmod=10;vverb=false;
+  drawbuffer=zeros(vvres,vhres,3);zbuffer=inf(vvres,vhres);
   for m = 1:size(vscene.objects,2)
     if (vverb) printf(['m[' num2str(m) ']: ']); endif
     drawobjtriangles = vscene.objects{m}.triangles;
@@ -14,7 +14,7 @@ function [k,d] = renderobjectrayscamera(vscene,vpos,vrays)
       smtriangle = drawobjtriangles(:,1:3,n); yhits = 0;
       linvrays = reshape(vrays,vhres*vvres,3);
       [lintrint,lintrhit,lintrdist,lintruvs,lintrhits]=raytriangleintersection(vpos,linvrays,smtriangle);
-      yhits = sum(lintrhit); drawindex = find((lintrhit==true)&(lintrdist>=0));
+      yhits = sum(lintrhit); drawindex = find((lintrhit==true)&(lintrdist>0));
       drawzindex = find(lintrdist(drawindex)<zbuffer(drawindex)); drawzcombindex = drawindex(drawzindex);
       zbuffer(drawzcombindex) = lintrdist(drawzcombindex);
       drawbuffer1=drawbuffer(:,:,1);drawbuffer2=drawbuffer(:,:,2);drawbuffer3=drawbuffer(:,:,3);
