58475f9f4c324bd46caef6c677f1d47629d93a8d
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-07 15:00:35 +0300

    updated renderobjectrayscamera
    added ray hit object, normal(stub), subsurface outputs.
    simple re-calculation removal optimizations

--- renderobjectrayscamera.m
+++ renderobjectrayscamera.m
@@ -1,6 +1,13 @@
-function [k,d] = renderobjectrayscamera(vscene,vpos,vrays)
+function [k,d,f,g,l] = renderobjectrayscamera(vscene,vpos,vrays,vnormal)
   vhres = size(vrays,2); vvres = size(vrays,1); distlmod=10;vverb=false;
+  linvrays = reshape(vrays,vhres*vvres,3); linvraysc = size(linvrays,1);
+  subsufracerays = zeros(vhres,vvres);
+  if ((!isempty(vnormal))&&(!isnan(vnormal)))
+    linvraysang = vectorangle(linvrays,ones(linvraysc,1).*vnormal(1:3));
+    subsufracerays = reshape(linvraysang>=90,vvres,vhres);
+  endif
   drawbuffer=zeros(vvres,vhres,3);zbuffer=inf(vvres,vhres);
+  obuffer=nan(vvres,vhres);tbuffer=nan(vvres,vhres);nbuffer=nan(vvres,vhres,3);
   for m = 1:size(vscene.objects,2)
     if (vverb) printf(['m[' num2str(m) ']: ']); endif
     drawobjtriangles = vscene.objects{m}.triangles;
@@ -12,11 +19,10 @@ function [k,d] = renderobjectrayscamera(vscene,vpos,vrays)
     ntriangles = size(drawobjtriangles,3);
     for n = 1:ntriangles
       smtriangle = drawobjtriangles(:,1:3,n); yhits = 0;
-      linvrays = reshape(vrays,vhres*vvres,3);
       [lintrint,lintrhit,lintrdist,lintruvs,lintrhits]=raytriangleintersection(vpos,linvrays,smtriangle);
       yhits = sum(lintrhit); drawindex = find((lintrhit==true)&(lintrdist>0));
       drawzindex = find(lintrdist(drawindex)<zbuffer(drawindex)); drawzcombindex = drawindex(drawzindex);
-      zbuffer(drawzcombindex) = lintrdist(drawzcombindex);
+      zbuffer(drawzcombindex)=lintrdist(drawzcombindex);obuffer(drawzcombindex)=m;tbuffer(drawzcombindex)=n;
       drawbuffer1=drawbuffer(:,:,1);drawbuffer2=drawbuffer(:,:,2);drawbuffer3=drawbuffer(:,:,3);
       drawbuffer1(drawzcombindex)=drawobjfacecolor(1);
       drawbuffer2(drawzcombindex)=drawobjfacecolor(2);
@@ -26,5 +32,5 @@ function [k,d] = renderobjectrayscamera(vscene,vpos,vrays)
     endfor
     if (vverb) printf(['\n']); endif
   endfor
-  k = drawbuffer; d = zbuffer;
+  k = drawbuffer; d = zbuffer; f = obuffer; f(:,:,2) = tbuffer; g = nbuffer; l = subsufracerays;
 endfunction
