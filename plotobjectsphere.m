5d16005cdf36e04e9e20b165747632be3b0011c9
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-07 09:18:35 +0300

    updated plotobjectsphere
    updated to faster arrays for line plotting

--- plotobjectsphere.m
+++ plotobjectsphere.m
@@ -6,10 +6,11 @@ function [k] = plotobjectsphere(vscene,vpos)
     objectx=drawobjtriangles(:,1,:)(:);objecty=drawobjtriangles(:,2,:)(:);objectz=objectm.triangles(:,3,:)(:);
     drawobjmaterial = vscene.materials{vscene.objects{m}.materialindex};
     drawobjfacecolor = [1 1 1]; if (isfield(drawobjmaterial,"facecolor")) drawobjfacecolor = drawobjmaterial.facecolor; endif
-    for n = 1:size(objectm.triangles,3)
-      smtriangle = objectm.triangles(:,:,n);
-      line(smtriangle([1 2 3 1],1),smtriangle([1 2 3 1],2),smtriangle([1 2 3 1],3),'color',drawobjfacecolor);
-    endfor
+    drawobjtx1=drawobjtriangles(1,1,:)(:);drawobjty1=drawobjtriangles(1,2,:)(:);drawobjtz1=drawobjtriangles(1,3,:)(:);
+    drawobjtx2=drawobjtriangles(2,1,:)(:);drawobjty2=drawobjtriangles(2,2,:)(:);drawobjtz2=drawobjtriangles(2,3,:)(:);
+    drawobjtx3=drawobjtriangles(3,1,:)(:);drawobjty3=drawobjtriangles(3,2,:)(:);drawobjtz3=drawobjtriangles(3,3,:)(:);
+    drawobjtxyz = [drawobjtx1 drawobjty1 drawobjtz1 drawobjtx2 drawobjty2 drawobjtz2 drawobjtx3 drawobjty3 drawobjtz3];
+    line(drawobjtxyz(:,[1 4 7 1]),drawobjtxyz(:,[2 5 8 2]),drawobjtxyz(:,[3 6 9 3]),'color',drawobjfacecolor);
     vsphere = pointcloudcircumsphere([objectx objecty objectz]);
     mesh(vsphere(4)*cx+vsphere(1),vsphere(4)*cy+vsphere(2),vsphere(4)*cz+vsphere(3),'edgecolor',[0.75 0.75 0.75],'facealpha',0.1,'linewidth',0.1);
   endfor
