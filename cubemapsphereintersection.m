19d7aeac529a1c76dcbf42aea8ce7662ed770bee
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-07 04:39:56 +0300

    updated cubemapsphereintersection
    updated angle ranges output

--- cubemapsphereintersection.m
+++ cubemapsphereintersection.m
@@ -5,6 +5,7 @@ function [k,d] = cubemapsphereintersection(vpos,vsphere,vres)
     cmres = vres; cmangles = cubemapangles(vres);
   endif
   lvecli=find(lvecl);lveclin=find(!lvecl);lveclic=size(lvecli,1);lveclinc=size(lveclin,1);
+  cmanglesa = ones(lveclic,1).*cmangles;
   for n = 1:6
     k{n,1} = nan(vspherec,2); k{n,1}(lveclin,:) = ones(lveclinc,1).*cmangles([1 end]);
     k{n,2} = nan(vspherec,2); k{n,2}(lveclin,:) = ones(lveclinc,1).*cmangles([1 end]);
@@ -24,9 +25,16 @@ function [k,d] = cubemapsphereintersection(vpos,vsphere,vres)
       lvecx1L = lvecx1a-cmradang; lvecx1H = lvecx1a+cmradang;
       lvecx2L = lvecx2a-cmradang; lvecx2H = lvecx2a+cmradang;
       lvecx1A = [lvecx1L lvecx1H]; lvecx2A = [lvecx2L lvecx2H];
-      k{n,1}(lvecli,:) = lvecx1A; k{n,2}(lvecli,:) = lvecx2A;
-      lvecx1Ah = sum((cmangles>=lvecx1L)&(cmangles<=lvecx1H),2);
-      lvecx2Ah = sum((cmangles>=lvecx2L)&(cmangles<=lvecx2H),2);
+      lvecx1I = (cmangles>=lvecx1L)&(cmangles<=lvecx1H);
+      lvecx2I = (cmangles>=lvecx2L)&(cmangles<=lvecx2H);
+      lvecx1If = find(lvecx1I); lvecx1Ifn = find(!lvecx1I);
+      lvecx2If = find(lvecx2I); lvecx2Ifn = find(!lvecx2I);
+      lvecx1Ia=cmanglesa; lvecx1Ia(lvecx1Ifn)=nan;
+      lvecx2Ia=cmanglesa; lvecx2Ia(lvecx2Ifn)=nan;
+      lvecx1Iaminmax = [min(lvecx1Ia,[],2) max(lvecx1Ia,[],2)];
+      lvecx2Iaminmax = [min(lvecx2Ia,[],2) max(lvecx2Ia,[],2)];
+      k{n,1}(lvecli,:) = lvecx1Iaminmax; k{n,2}(lvecli,:) = lvecx2Iaminmax;
+      lvecx1Ah = sum(lvecx1I,2); lvecx2Ah = sum(lvecx2I,2);
       lvechiti = find((lvecx1Ah>0)|(lvecx2Ah>0));
       if (!isempty(lvechiti)) d(lvecli(lvechiti),1) = true; endif
     endfor
