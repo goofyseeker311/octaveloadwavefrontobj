0afcefe1c33c41ceec9b896a9183276693d26b5c
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-07 09:42:46 +0300

    updated plotobjectsphere
    changed to still not accurate faster vector plotting mode

--- plotobjectsphere.m
+++ plotobjectsphere.m
@@ -7,7 +7,7 @@ function [k] = plotobjectsphere(vscene,vpos)
     drawobjmaterial = vscene.materials{vscene.objects{m}.materialindex};
     drawobjfacecolor = [1 1 1]; if (isfield(drawobjmaterial,"facecolor")) drawobjfacecolor = drawobjmaterial.facecolor; endif
     objectxyz = [objectx objecty objectz]; vsphere = pointcloudcircumsphere(objectxyz);
-    line(objectxyz(:,1),objectxyz(:,2),objectxyz(:,3),'color',drawobjfacecolor);
+    line("xdata",objectxyz(:,1),"ydata",objectxyz(:,2),"zdata",objectxyz(:,3),'color',drawobjfacecolor);
     mesh(vsphere(4)*cx+vsphere(1),vsphere(4)*cy+vsphere(2),vsphere(4)*cz+vsphere(3),'edgecolor',[0.75 0.75 0.75],'facealpha',0.1,'linewidth',0.1);
   endfor
   hold off;
