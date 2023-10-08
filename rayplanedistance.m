a88d94fa29a4280583fe7a9e27f9d74317434e42
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-08 11:33:58 +0300

    updated rayplanedistance
    switched planes to fast arrays and directions to slow loop.

--- rayplanedistance.m
+++ rayplanedistance.m
@@ -2,10 +2,10 @@ function [k] = rayplanedistance(vpos,vdir,vplane)
   vdirc = size(vdir,1); vplanec = size(vplane,1); k = nan;
   if ((!isempty(vpos))&&(vdirc>0)&&(vplanec>0))
     k = nan(vdirc,vplanec);
-    for n = 1:vplanec
-      top = -dot([vpos 1],vplane(n,:),2);
-      bottom = dot(vdir,ones(vdirc,1).*vplane(n,1:3),2);
-      k(:,n) = top./bottom;
+    for n = 1:vdirc
+      top = -dot(ones(vplanec,1).*[vpos 1],vplane,2);
+      bottom = dot(ones(vplanec,1).*vdir(n,:),vplane(:,1:3),2);
+      k(n,:) = top./bottom;
     endfor
   endif
 endfunction
