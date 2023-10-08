d1aac3b415281e147fa0e064b8dd383fe8fde7bc
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-08 07:49:22 +0300

    updated renderobjectrayscamera
    added ray hits normal buffer output

--- renderobjectrayscamera.m
+++ renderobjectrayscamera.m
@@ -2,7 +2,7 @@ function [k,d,f,g,p] = renderobjectrayscamera(vscene,vpos,vrays)
   vhres = size(vrays,2); vvres = size(vrays,1); distlmod=10;vverb=false;
   linvrays = reshape(vrays,vhres*vvres,3);
   drawbuffer=zeros(vvres,vhres,3);zbuffer=inf(vvres,vhres);
-  obuffer=nan(vvres,vhres);tbuffer=nan(vvres,vhres);nbuffer=nan(vvres,vhres,3);
+  obuffer=nan(vvres,vhres);tbuffer=nan(vvres,vhres);nbuffer=nan(vvres,vhres,4);
   for m = 1:size(vscene.objects,2)
     if (vverb) printf(['m[' num2str(m) ']: ']); endif
     drawobjtriangles = vscene.objects{m}.triangles;
@@ -13,15 +13,17 @@ function [k,d,f,g,p] = renderobjectrayscamera(vscene,vpos,vrays)
     endif
     ntriangles = size(drawobjtriangles,3);
     for n = 1:ntriangles
-      smtriangle = drawobjtriangles(:,1:3,n); yhits = 0;
+      smtriangle = drawobjtriangles(:,1:3,n); smtrianglef = drawobjtriangles(:,:,n); yhits = 0;
       [lintrint,lintrhit,lintrdist,lintruvs,lintrhits]=raytriangleintersection(vpos,linvrays,smtriangle);
       yhits = sum(lintrhit); drawindex = find((lintrhit==true)&(lintrdist>0));
       drawzindex = find(lintrdist(drawindex)<zbuffer(drawindex)); drawzcombindex = drawindex(drawzindex);
       zbuffer(drawzcombindex)=lintrdist(drawzcombindex);obuffer(drawzcombindex)=m;tbuffer(drawzcombindex)=n;
+      hitnormal = planefromnormalatpoint(smtrianglef(1,1:3),smtrianglef(1,6:8));
+      nbuffer1=nbuffer(:,:,1);nbuffer2=nbuffer(:,:,2);nbuffer3=nbuffer(:,:,3);nbuffer4=nbuffer(:,:,4);
+      nbuffer1(drawzcombindex)=hitnormal(1);nbuffer2(drawzcombindex)=hitnormal(2);nbuffer3(drawzcombindex)=hitnormal(3);nbuffer4(drawzcombindex)=hitnormal(4);
+      nbuffer(:,:,1)=nbuffer1;nbuffer(:,:,2)=nbuffer2;nbuffer(:,:,3)=nbuffer3;nbuffer(:,:,4)=nbuffer4;
       drawbuffer1=drawbuffer(:,:,1);drawbuffer2=drawbuffer(:,:,2);drawbuffer3=drawbuffer(:,:,3);
-      drawbuffer1(drawzcombindex)=drawobjfacecolor(1);
-      drawbuffer2(drawzcombindex)=drawobjfacecolor(2);
-      drawbuffer3(drawzcombindex)=drawobjfacecolor(3);
+      drawbuffer1(drawzcombindex)=drawobjfacecolor(1);drawbuffer2(drawzcombindex)=drawobjfacecolor(2);drawbuffer3(drawzcombindex)=drawobjfacecolor(3);
       drawbuffer(:,:,1)=drawbuffer1;drawbuffer(:,:,2)=drawbuffer2;drawbuffer(:,:,3)=drawbuffer3;
       if (vverb) printf([' ' num2str(yhits)]); endif
     endfor
