642b6567277f9e1df70c3dc97a84dc08510c5b41
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-08 15:43:40 +0300

    updated planetriangleintersection
    switched to using rayplanedistance, and updated to support multiple stacked planes (fast array) and triangles (slow loop).
    updated output to be horizontal arrays (in cells if multiple triangles), breaks anything relying on previous form of single stacked parameter output.

--- planetriangleintersection.m
+++ planetriangleintersection.m
@@ -1,15 +1,29 @@
 function [k,d,u,f] = planetriangleintersection(vplane,vtriangle)
-  k = nan; d = false; u = nan(2,1); f = nan(2,1);
-  [ptlint1,ptlhit1,ptluv1] = planelineintersection(vplane,vtriangle([1 2],:));
-  [ptlint2,ptlhit2,ptluv2] = planelineintersection(vplane,vtriangle([1 3],:));
-  [ptlint3,ptlhit3,ptluv3] = planelineintersection(vplane,vtriangle([2 3],:));
-  if (ptlhit1||ptlhit2||ptlhit3)
-    ptint = [];
-    if (ptlhit1) ptint = [ptint;ptlint1 ptluv1 ones(size(ptlint1,1),1)*[1,2]]; endif
-    if (ptlhit2) ptint = [ptint;ptlint2 ptluv2 ones(size(ptlint2,1),1)*[1,3]]; endif
-    if (ptlhit3) ptint = [ptint;ptlint3 ptluv3 ones(size(ptlint3,1),1)*[2,3]]; endif
-    [ptuint,ptuind] = unique(ptint(:,1:4),"rows"); puintf = [ptint(ptuind,:)];
-    if (size(puintf,1)) puintf = [puintf;puintf]; endif
-    k = puintf(:,1:3); d = true; u = puintf(:,4); f = puintf(:,5:6);
+  k={}; d=[]; u={}; f={}; vplanec=size(vplane,1); vtrianglec=size(vtriangle,3);
+  array12=ones(vplanec,1).*[1 2];array13=ones(vplanec,1).*[1 3];array23=ones(vplanec,1).*[2 3];
+  for n = 1:vtrianglec
+    vtri12=vtriangle(2,:,n)-vtriangle(1,:,n);vtri13=vtriangle(3,:,n)-vtriangle(1,:,n);vtri23=vtriangle(3,:,n)-vtriangle(2,:,n);
+    ptd1=rayplanedistance(vtriangle(1,:,n),vtri12,vplane)';ptlint1=vtriangle(1,:,n)+ptd1.*vtri12;ptlhit1=(ptd1>0)&(ptd1<1);
+    ptd2=rayplanedistance(vtriangle(1,:,n),vtri13,vplane)';ptlint2=vtriangle(1,:,n)+ptd2.*vtri13;ptlhit2=(ptd2>0)&(ptd2<1);
+    ptd3=rayplanedistance(vtriangle(2,:,n),vtri23,vplane)';ptlint3=vtriangle(2,:,n)+ptd3.*vtri23;ptlhit3=(ptd3>0)&(ptd3<1);
+    pthit = find(ptlhit1|ptlhit2|ptlhit3); phits = [ptlhit1 ptlhit2 ptlhit3];
+    ptlint = nan(vplanec,6); ptluv = nan(vplanec,2); ptlind = nan(vplanec,4);
+    if (!isempty(pthit))
+      phitsa=phits(pthit,:);
+      phitsa12=find(phitsa(:,1)&phitsa(:,2));phitsa13=find(phitsa(:,1)&phitsa(:,3));phitsa23=find(phitsa(:,2)&phitsa(:,3));
+      ptlint(pthit(phitsa12),1:3) = ptlint1(pthit(phitsa12),:); ptlint(pthit(phitsa12),4:6) = ptlint2(pthit(phitsa12),:);
+      ptlint(pthit(phitsa13),1:3) = ptlint1(pthit(phitsa13),:); ptlint(pthit(phitsa13),4:6) = ptlint3(pthit(phitsa13),:);
+      ptlint(pthit(phitsa23),1:3) = ptlint2(pthit(phitsa23),:); ptlint(pthit(phitsa23),4:6) = ptlint3(pthit(phitsa23),:);
+      ptluv(pthit(phitsa12),1) = ptd1(pthit(phitsa12),:); ptluv(pthit(phitsa12),2) = ptd2(pthit(phitsa12),:);
+      ptluv(pthit(phitsa13),1) = ptd1(pthit(phitsa13),:); ptluv(pthit(phitsa13),2) = ptd3(pthit(phitsa13),:);
+      ptluv(pthit(phitsa23),1) = ptd2(pthit(phitsa23),:); ptluv(pthit(phitsa23),2) = ptd3(pthit(phitsa23),:);
+      ptlind(pthit(phitsa12),1:2) = array12(pthit(phitsa12),:); ptlind(pthit(phitsa12),3:4) = array13(pthit(phitsa12),:);
+      ptlind(pthit(phitsa13),1:2) = array12(pthit(phitsa13),:); ptlind(pthit(phitsa13),3:4) = array23(pthit(phitsa13),:);
+      ptlind(pthit(phitsa23),1:2) = array13(pthit(phitsa23),:); ptlind(pthit(phitsa23),3:4) = array23(pthit(phitsa23),:);
+    endif
+    k{n} = ptlint; d(:,n) = pthit; u{n} = ptluv; f{n} = ptlind;
+  endfor
+  if (vtrianglec==1)
+    k = k{1}; u = u{1}; f = f{1};
   endif
 endfunction
