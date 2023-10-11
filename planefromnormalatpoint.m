f9229e4797f4361cb5d11b63bfc99dcaea9da326
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-11 17:04:03 +0300

    updated planefromnormalatpoint
    changed to single or paired input rows.

--- planefromnormalatpoint.m
+++ planefromnormalatpoint.m
@@ -1,10 +1,6 @@
 function [k] = planefromnormalatpoint(vpoint,vnormal)
   vpointc = size(vpoint,1); vnormalc = size(vnormal,1); k = nan;
   if ((vpointc>0)&&(vnormalc>0))
-    k = nan(vpointc,4,vnormalc);
-    for n = 1:vnormalc
-      nm=ones(vpointc,1).*normalizevector(vnormal(n,:)); dv=-dot(nm,vpoint,2);
-      k(:,:,n) = [nm dv];
-    endfor
+    nm=normalizevector(vnormal); dv=-dot(nm,vpoint,2); k = [nm dv];
   endif
 endfunction
