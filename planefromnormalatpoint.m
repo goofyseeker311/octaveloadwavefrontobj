2ade6a3ce8b5e30b0b9e7c3cc77135a238911fa4
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-12 13:35:04 +0300

    updated planefromnormalatpoint
    added array size match check.

--- planefromnormalatpoint.m
+++ planefromnormalatpoint.m
@@ -1,6 +1,6 @@
 function [k] = planefromnormalatpoint(vpoint,vnormal)
   vpointc = size(vpoint,1); vnormalc = size(vnormal,1); k = nan;
-  if ((vpointc>0)&&(vnormalc>0))
+  if ((vpointc>0)&&(vnormalc>0)&&(vpointc==vnormalc))
     nm=normalizevector(vnormal); dv=-dot(nm,vpoint,2); k = [nm dv];
   endif
 endfunction
