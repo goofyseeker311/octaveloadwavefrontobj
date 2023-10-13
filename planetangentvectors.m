6e2e2527735e24ee8d7f7b35afb71b132427edd6
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-13 11:48:53 +0300

    updated planetangentvectors
    added support for multiple stacked position and planes pairs (fast array).
    updated test line.

--- planetangentvectors.m
+++ planetangentvectors.m
@@ -1,6 +1,6 @@
 function [k] = planetangentvectors(vpos,vplane)
   v1=vplane(:,[2 3 1]);
-  s1=planefromnormalatpoint(vpos,normalizevector(cross(v1,vplane(:,1:3))));
-  s2=planefromnormalatpoint(vpos,normalizevector(cross(s1(1:3),vplane(:,1:3))));
+  s1=planefromnormalatpoint(vpos,normalizevector(cross(v1,vplane(:,1:3),2)));
+  s2=planefromnormalatpoint(vpos,normalizevector(cross(s1(:,1:3),vplane(:,1:3),2)));
   k=s1; k(:,:,2)=s2;
 endfunction
