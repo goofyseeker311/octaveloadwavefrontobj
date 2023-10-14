787959605676f23e5e3a6608ced72afca7259a9e
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-14 10:13:02 +0300

    updated planetangentvectors
    updated to better algorithm that works in all cases.
    changed objreader reserved campos variable to something else.

--- objreader.m
+++ objreader.m
@@ -1,68 +1,68 @@
 clear all; objfilename="testcubemodel4.obj"; #format long;output_precision(16); #testcubemodel4
 hres = 1920; vres = 1080; hfov = 90; vfov = 67.5; #pvrot = [0 0 0];
-campos = [-2 0.5 1.5]; camdir = normalizevector([1 0 -0.4]);
+campost = [-2 0.5 1.5]; camdir = normalizevector([1 0 -0.4]);
 camrgt = [0 -1 0]; camup = normalizevector(cross(camrgt,camdir));
-camplane = planefromnormalatpoint(campos,camup);
+camplane = planefromnormalatpoint(campost,camup);
 triangle = [1 1 0;1 0 1;1 0 0];
 veclen = vectorlength(camdir);
 cubemodel = loadwavefrontobjfile(objfilename);
 trplane = planefrompoints(triangle);
-ppdist = pointplanedistance(campos,trplane);
-rpdist = rayplanedistance(campos,camdir,trplane);
+ppdist = pointplanedistance(campost,trplane);
+rpdist = rayplanedistance(campost,camdir,trplane);
 [trsphere] = trianglecircumsphere(triangle);
 [pcsphere] = pointcloudcircumsphere(cubemodel.vertexlist(1:8,:));
 sgn = signnum([-1 0 1]);
-vecang = vectorangle(camdir,campos);
-[trint,trhit,trdist,truvs,trhits]=raytriangleintersection(campos,camdir,triangle);
-[cubeint,cubehit,cubedist] = cubemapsphereintersection(campos,pcsphere,vres);
-[sphereint,spherehit,spheredist] = spheremapsphereintersection(campos,pcsphere,hres,vres);
+vecang = vectorangle(camdir,campost);
+[trint,trhit,trdist,truvs,trhits]=raytriangleintersection(campost,camdir,triangle);
+[cubeint,cubehit,cubedist] = cubemapsphereintersection(campost,pcsphere,vres);
+[sphereint,spherehit,spheredist] = spheremapsphereintersection(campost,pcsphere,hres,vres);
 [ptint,pthit,ptuvs,pthits] = planetriangleintersection(camplane,triangle);
 [rlint,rlhit,rldist] = raylineintersection([0 0 -0.1],[1 0 0],[triangle(2,1:3) triangle(3,1:3)]);
 [psint,pshit,psdist,psrad] = planesphereintersection(camplane,pcsphere+[0 0 2 0]);
 [cmanglelist,cmsteplist] = cubemapangles(vres);
 [smhanglelist,smvanglelist] = spheremapangles(hres,vres);
 [hangles,hstep,vangles,vstep,dasp,aasp]=projectedangles(hres,vres,hfov,vfov);
-[cmplanes,cmpd,cmpu,cmpr] = cubemapplanes(campos,vres);
-[smplanes,smpd,smpu] = spheremapplanes(campos,hres);
-prplanes = projectedplanes(campos,hres,vres,hfov,vfov);
-camptvecs = planetangentvectors(campos,camplane);
+[cmplanes,cmpd,cmpu,cmpr] = cubemapplanes(campost,vres);
+[smplanes,smpd,smpu] = spheremapplanes(campost,hres);
+prplanes = projectedplanes(campost,hres,vres,hfov,vfov);
+camptvecs = planetangentvectors(campost,camplane);
 [dbout,zbout]=mergedrawbuffers(zeros(2,2,3),[1 1;2 2],ones(2,2,3),[0.5 1.5;2.5 1.5]);
 projrays = projectedrays(hres,vres,hfov,vfov);
 spheremaprays = equilateralspheremaprays(hres,vres);
 cubemaprays = unitxyzcubemaprays(vres);
 subsrays = subsurfacerays(spheremaprays,trplane);
 
-figure(1);clf;whitebg([0.8 0.8 0.8]);plotobjectsphere(cubemodel,campos);view(3);axis equal;
+figure(1);clf;whitebg([0.8 0.8 0.8]);plotobjectsphere(cubemodel,campost);view(3);axis equal;
 saveas(1,"sceneobjectsrenderA.png");
 
-[ssdbuffer,sszbuffer] = renderobjectspheresketch(cubemodel,campos,hres,vres);
+[ssdbuffer,sszbuffer] = renderobjectspheresketch(cubemodel,campost,hres,vres);
 figure(2);clf;imagesc(ssdbuffer);whitebg([0.8 0.8 0.8]);xlim([1 hres]);ylim([1 vres]);axis off;daspect([1 1]);
 imwrite(ssdbuffer,['spheremapsketchrenderA.png']);
 
-[csdbuffer,cszbuffer] = renderobjectcubesketch(cubemodel,campos,vres);
+[csdbuffer,cszbuffer] = renderobjectcubesketch(cubemodel,campost,vres);
 for n = 1:6
   figure(n+2);clf;imagesc(csdbuffer{n});whitebg([0.8 0.8 0.8]);xlim([1 vres]);ylim([1 vres]);axis off;daspect([1 1]);
   imwrite(csdbuffer{n},['cubemapsketchrenderA' num2str(n) '.png']);
 endfor
-[scdbuffer,sczbuffer] = renderobjectspherecamera(cubemodel,campos,hres,vres);
+[scdbuffer,sczbuffer] = renderobjectspherecamera(cubemodel,campost,hres,vres);
 figure(9);clf;imagesc(scdbuffer);whitebg([0.8 0.8 0.8]);xlim([1 hres]);ylim([1 vres]);axis off;daspect([1 1]);
 imwrite(scdbuffer,['spheremapplanerenderA.png']);
-[cmdbuffers,cmzbuffers] = renderobjectcubecamera(cubemodel,campos,vres);
+[cmdbuffers,cmzbuffers] = renderobjectcubecamera(cubemodel,campost,vres);
 for n = 1:6
   figure(n+9);clf;imagesc(cmdbuffers{n});whitebg([0.8 0.8 0.8]);xlim([1 vres]);ylim([1 vres]);axis off;daspect([1 1]);
   imwrite(cmdbuffers{n},['cubemapplanerenderA' num2str(n) '.png']);
 endfor
 
-[smrdbuffer,smrzbuffer] = renderobjectrayscamera(cubemodel,campos,spheremaprays);
+[smrdbuffer,smrzbuffer] = renderobjectrayscamera(cubemodel,campost,spheremaprays);
 figure(16);clf;imagesc(smrdbuffer);whitebg([0.8 0.8 0.8]);xlim([1 hres]);ylim([1 vres]);axis off;daspect([1 1]);
 imwrite(smrdbuffer,['spheremapraysrenderA.png']);
 for n = 1:6
-  [cmrdbuffer,cmrzbuffer] = renderobjectrayscamera(cubemodel,campos,cubemaprays{n});
+  [cmrdbuffer,cmrzbuffer] = renderobjectrayscamera(cubemodel,campost,cubemaprays{n});
   figure(n+16);clf;imagesc(cmrdbuffer);whitebg([0.8 0.8 0.8]);xlim([1 vres]);ylim([1 vres]);axis off;daspect([1 1]);
   imwrite(cmrdbuffer,['cubemapraysrenderA' num2str(n) '.png']);
 endfor
 
-[prrdbuffer,prrzbuffer,objbuffer,normbuffer,pointbuffer] = renderobjectrayscamera(cubemodel,campos,projrays);
+[prrdbuffer,prrzbuffer,objbuffer,normbuffer,pointbuffer] = renderobjectrayscamera(cubemodel,campost,projrays);
 figure(23);clf;imagesc(prrdbuffer);whitebg([0.8 0.8 0.8]);xlim([1 hres]);ylim([1 vres]);axis off;daspect([1 1]);
 imwrite(prrdbuffer,['projectedraysrenderA.png']);
 bordbuffer = renderobjectraysbouncecamera(cubemodel,pointbuffer,spheremaprays,objbuffer,normbuffer);
--- planetangentvectors.m
+++ planetangentvectors.m
@@ -1,5 +1,5 @@
 function [k] = planetangentvectors(vpos,vplane)
-  v1=vplane(:,[2 3 1]);
+  v1=[-vplane(:,2) vplane(:,3) vplane(:,1)];
   s1=planefromnormalatpoint(vpos,normalizevector(cross(v1,vplane(:,1:3),2)));
   s2=planefromnormalatpoint(vpos,normalizevector(cross(s1(:,1:3),vplane(:,1:3),2)));
   k=s1; k(:,:,2)=s2;
