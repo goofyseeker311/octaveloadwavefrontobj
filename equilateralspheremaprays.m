8b97b8770a9d3428867d7e07c35a18e521d0acf3
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-18 16:40:02 +0300

    updated equilateralspheremaprays
    changed default looking center view direction of the rays to forward instead of right.

--- equilateralspheremaprays.m
+++ equilateralspheremaprays.m
@@ -1,6 +1,6 @@
 function [k] = equilateralspheremaprays(hres,vres)
   k = []; hanglestep = 360./(hres-1); vanglestep = 180./(vres-1);
-  hanglelist = -180:hanglestep:180; vanglelist = -90:vanglestep:90;
+  hanglelist = -90:hanglestep:270; vanglelist = -90:vanglestep:90;
   vvectors = [cosd(vanglelist);zeros(1,size(vanglelist,2));sind(vanglelist)];
   rvectors = zeros(3,vres,hres);
   for n = 1:hres
