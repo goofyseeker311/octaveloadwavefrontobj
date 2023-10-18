4ef169b0441f983bf8768e46944ba2a90114f653
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-18 13:00:15 +0300

    updated pointplanedistance
    swapped fast array processing based on input array size, supporting both parameters fast array processing.
    if both input are arrays then slow loop over the points while planes use fast arrays.

--- pointplanedistance.m
+++ pointplanedistance.m
@@ -2,8 +2,12 @@ function [k] = pointplanedistance(vpoint,vplane)
   k = nan; pointc=size(vpoint,1); planec=size(vplane,1);
   if ((pointc>0)&&(planec>0))
     k = nan(pointc,planec); l2=vectorlength(vplane(:,1:3));
-    for n = 1:pointc
-      k(n,:)=dot(ones(planec,1).*[vpoint(n,:) 1],vplane,2)./l2;
-    endfor
+    if (planec>1)
+      for n = 1:pointc
+        k(n,:)=dot(ones(planec,1).*[vpoint(n,:) 1],vplane,2)./l2;
+      endfor
+    else
+      k(:,1)=dot([vpoint ones(pointc,1)],ones(pointc,1).*vplane,2)./l2;
+    endif
   endif
 endfunction
