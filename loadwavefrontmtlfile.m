e19dc5b9d34d1b1eb6f2436ae4118df921c4633a
goofyseeker311 <goofyseeker311@gmail.com>
2023-10-23 17:23:21 +0300

    updated loadwavefrontmtlfile
    load texture to materials{n}.fileimage

--- loadwavefrontmtlfile.m
+++ loadwavefrontmtlfile.m
@@ -11,6 +11,7 @@ function [k] = loadwavefrontmtlfile(fname)
     elseif (strncmp(lower(fline),"map_kd ",7))
       farg = strtrim(substr(fline, 8));
       k.materials{materialcount}.filename = farg;
+      k.materials{materialcount}.fileimage = imread(farg);
     elseif (strncmp(lower(fline),"kd ",3))
       farg = strtrim(substr(fline, 4));
       k.materials{materialcount}.facecolor = str2num(farg);
